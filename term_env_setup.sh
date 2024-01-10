aadfd603842e3e34ce64f05980da338e3fd382f6
João Bordalo <joao-bordalo@hotmail.com>
2024-01-10 23:12:45 +0000

    Update terminal and environment setup script
    Still not very tested

--- term_env_setup.sh
+++ term_env_setup.sh
@@ -10,9 +10,16 @@ sudo apt install -y tmux
 # Install neovim
 sudo apt install -y neovim
 
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 # Install Alacritty
 
-# Dependencies
+## Rust
+sudo apt install curl
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+## Dependencies
 sudo apt-get install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 
 mkdir ~/.local/bin
@@ -22,6 +29,9 @@ cd alacritty
 cargo build --release
 
 sudo cp target/release/alacritty /usr/local/bin
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
 
 cd ~
 
@@ -38,6 +48,9 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/.git --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
 config checkout
 
+# WARN Untested
+config reset --hard
+
 # Set as default terminal
 
-sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/alacritty 50
+#sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/alacritty 50
